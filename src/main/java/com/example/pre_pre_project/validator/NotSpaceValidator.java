fb11001da6e3fa384b1012979a21de3571643a0a
jonghyuk <pjongk148@gmail.com>
2022-11-22 21:27:40 +0900

    Validation 추가

---
+++ src/main/java/com/example/pre_pre_project/validator/NotSpaceValidator.java
@@ -0,0 +1,21 @@
+package com.example.pre_pre_project.validator;
+
+import org.springframework.util.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.Annotation;
+
+public class NotSpaceValidator implements ConstraintValidator<NotSpace, String> {
+
+    @Override
+    public void initialize(NotSpace constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return value == null || StringUtils.hasText(value);
+    }
+
+}
